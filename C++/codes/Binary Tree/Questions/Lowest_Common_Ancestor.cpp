ba8c0595f63791a7dc857edba5fa314208091c70
SplinterSword <57327315+SplinterSword@users.noreply.github.com>
2024-07-17 16:42:36 +0530

    DSA

--- C++/codes/Binary Tree/Questions/Lowest_Common_Ancestor.cpp
+++ C++/codes/Binary Tree/Questions/Lowest_Common_Ancestor.cpp
@@ -1,4 +1,17 @@
 // question link is in lecture no. 64  
-// https://practice.geeksforgeeks.org/problems/sum-of-the-longest-bloodline-of-a-tree/1
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
 // S.C. = O(n)
 // T.C. = O(height)
+
+class Solution {
+ public:
+  TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (root == nullptr || root == p || root == q)
+      return root;
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+    if (left != nullptr && right != nullptr)
+      return root;
+    return left == nullptr ? right : left;
+  }
+};
--- C++/codes/Binary Tree/Questions/Sum_of_Longest_Path.cpp
+++ C++/codes/Binary Tree/Questions/Sum_of_Longest_Path.cpp
@@ -2,3 +2,42 @@
 // https://practice.geeksforgeeks.org/problems/sum-of-the-longest-bloodline-of-a-tree/1
 // S.C. = O(n)
 // T.C. = O(n)
+
+class Solution
+{
+private:
+    void solve(Node *root,int &path,int tempPath,int &sum,int tempSum){
+        if (root == NULL){
+            if (tempPath>path){
+                path = tempPath;
+                sum = tempSum;
+            }
+            else if (path == tempPath && sum < tempSum){
+                sum = tempSum;
+            }
+            return;
+        }
+        
+
+        solve(root->left,path,tempPath+1,sum,tempSum+root->data);
+        solve(root->right,path,tempPath+1,sum,tempSum+root->data);
+        
+        return;
+        
+    }
+public:
+    
+    int sumOfLongRootToLeafPath(Node *root)
+    {
+        //code here
+        if (root == NULL){
+            return 0;
+        }
+        
+        int sum = INT_MIN;
+        int path = 0;
+        solve(root,path,0,sum,0);
+        return sum;
+        
+    }
+};
