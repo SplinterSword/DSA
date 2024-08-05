d2dba9d502015b9d629ae70c4c783f30cbf5fa96
SplinterSword <sparshj2003@gmail.com>
2024-08-05 19:44:05 +0530

    DSA

--- C++/codes/Graphs/Code/implementation.cpp
+++ C++/codes/Graphs/Code/implementation.cpp
@@ -2,70 +2,69 @@
 
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 #include <list>
+#include <unordered_map>
 using namespace std;
 
 class graph {
+    private:
+        unordered_map<int,list<int>> adjList;
     public:
-    unordered_map<int,list<int>> adj;
 
-    void addEdge(int u,int v, bool direction) {
-        // direction = 0 -> undirected
-        // direction = 1 -> directed graph
-
-        // create an edge from u to v
-        adj[u].push_back(v);
-        if (direction == 0){
-            adj[v].push_back(u);
-        }
-    }
-
-    void printAdjList() {
-        for (auto i:adj){
-            cout << i.first << "->";
-            for (auto j: i.second) {
-                cout << j << ", ";
+        void addEdgeList(int u,int v,bool direction){
+            // direction = 0 for undirected
+            // direction = 1 for directd graph
+            adjList[u].push_back(v);
+            if (direction == 0){
+                adjList[v].push_back(u);
             }
-            cout << endl;
         }
-    }
+
+        void printGraph(){
+
+            // List Part
+            for (auto i:adjList){
+                cout << i.first << "->";
+                for (auto j:i.second){
+                    cout << j << ", ";
+                }
+                cout << endl;
+            }
+
+        }
 };
 
-int main(){
-    
+int main() {
     int vertices;
     cout << "Enter the number of vertices: ";
     cin >> vertices;
 
     int edges;
-    cout << "Enter the number of edges: ";
+    cout << "Enter the number of Edges: ";
     cin >> edges;
+
     vector<vector<int>> adjMatrix(vertices,vector<int>(vertices,0));
+    cout << "Enter the edges:\n";
     graph g;
 
     for (int i=0;i<edges;i++){
-        int u, v;
+        int u,v;
         cin >> u >> v;
-
+        g.addEdgeList(u,v,0);
         adjMatrix[u][v] = 1;
         adjMatrix[v][u] = 1;
-
-        // Creating an undirected graph (adjList)
-        g.addEdge(u,v,0);
     }
 
-    //printing a graph
-    g.printAdjList();
+    g.printGraph();
 
-    // printing adjcency matrix
-    for (int i=0;i<vertices;i++){
-        cout << i << "-> ";
-        for (int j=0;j<vertices;j++){
-            if (adjMatrix[i][j] == 1){
-                cout << j << ", ";
+    // Matrix Part
+        for (int i=0;i<adjMatrix.size();i++){
+            cout << i << "->";
+            for (int j=0;j<adjMatrix.size();j++){
+                if (adjMatrix[i][j] == 1){
+                    cout << j << ", ";
+                }
             }
+            cout << endl;
         }
-        cout << endl;
-    }
 }
