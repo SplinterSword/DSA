cbf1d994306a873593fcbdb1cde28a26ee0e0eab
SplinterSword <sparshj2003@gmail.com>
2025-06-19 13:39:08 +0530

    Revision Done Segment Tree

--- C++/codes/Segment Tree/Revision.cpp
+++ C++/codes/Segment Tree/Revision.cpp
@@ -1,19 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void buildSegmentTree(int *arr, int *segTree, int index, int start, int end) {
+void buildSegmentTree(int *arr, int *segTree, int start, int end, int index) {
     if (start > end) return;
+
     if (start == end) {
         segTree[index] = arr[start];
         return;
     }
+
     int mid = (start + end) / 2;
-    buildSegmentTree(arr, segTree, 2*index+1, start, mid);
-    buildSegmentTree(arr, segTree, 2*index+2, mid+1, end);
+    buildSegmentTree(arr,segTree,start,mid,2*index+1);
+    buildSegmentTree(arr,segTree,mid+1,end,2*index+2);
     segTree[index] = segTree[2*index+1] + segTree[2*index+2];
     return;
 }
-
 
 int querySegmentTree(int *segTree, int index, int start, int end, int requiredStart, int requiredEnd) {
     // completely inside
@@ -25,15 +26,19 @@ int querySegmentTree(int *segTree, int index, int start, int end, int requiredSt
     if (start > requiredEnd || end < requiredStart) {
         return 0;
     }
-    
-    // Overlap both Partial and Fully
+
+    // overlapping partially or fully
     int mid = (start + end) / 2;
-    int left = querySegmentTree(segTree, 2*index+1, start, mid, requiredStart, requiredEnd);
-    int right = querySegmentTree(segTree, 2*index+2, mid + 1, end, requiredStart, requiredEnd);
+
+    int left = querySegmentTree(segTree,2*index+1,start,mid,requiredStart,requiredEnd);
+    int right = querySegmentTree(segTree,2*index+2,mid+1,end,requiredStart,requiredEnd);
+
     return left + right;
 }
 
-void pointUpdate(int *segTree, int index , int start, int end, int updateIndex, int updateValue) {
+void pointUpdate (int *segTree, int index , int start, int end, int updateIndex, int updateValue){
+    if (start > end) return;
+
     if (start == end && start == updateIndex) {
         segTree[index] += updateValue;
         return;
@@ -41,11 +46,11 @@ void pointUpdate(int *segTree, int index , int start, int end, int updateIndex, 
 
     int mid = (start + end) / 2;
     
-    if (updateIndex > mid) {
-        pointUpdate(segTree, 2*index+2, mid + 1, end, updateIndex, updateValue);
-    }
+    if (updateIndex <= mid) {
+        pointUpdate(segTree,2*index+1,start,mid,updateIndex,updateValue);
+    } 
     else {
-        pointUpdate(segTree, 2*index+1, start, mid, updateIndex, updateValue);
+        pointUpdate(segTree,2*index+2,mid+1,end,updateIndex,updateValue);
     }
 
     segTree[index] = segTree[2*index+1] + segTree[2*index+2];
@@ -53,23 +58,29 @@ void pointUpdate(int *segTree, int index , int start, int end, int updateIndex, 
     return;
 }
 
-// Lazy Propogation
+// lazy propogation
 
 void rangeUpdate(int *segTree, int *lazyTree, int index, int start, int end, int requiredStart, int requiredEnd, int updateValue) {
-    
-    //check for suspended update and if there update
+    // handle lazy
     if (lazyTree[index] != 0) {
-        segTree[index] = (end - start + 1) * lazyTree[index];
+        int range = end - start + 1;
+        int value = range * lazyTree[index];
+        segTree[index] += value;
+
         if (start != end) {
             lazyTree[2*index+1] += lazyTree[index];
             lazyTree[2*index+2] += lazyTree[index];
         }
+
         lazyTree[index] = 0;
     }
 
     // completely inside
-    if (requiredStart <= start && requiredEnd >= end) {
-        segTree[index] = (end - start + 1) * updateValue;
+    if (start >= requiredStart && end <= requiredEnd) {
+        int range = end - start + 1;
+        int value = range * updateValue;
+        segTree[index] += value;
+
         if (start != end) {
             lazyTree[2*index+1] += updateValue;
             lazyTree[2*index+2] += updateValue;
@@ -81,7 +92,7 @@ void rangeUpdate(int *segTree, int *lazyTree, int index, int start, int end, int
     if (start > requiredEnd || end < requiredStart) {
         return;
     }
-    
+
     // Overlap both Partial and Fully
     int mid = (start + end) / 2;
     rangeUpdate(segTree, lazyTree, 2*index+1, start, mid, requiredStart, requiredEnd, updateValue);
@@ -91,19 +102,22 @@ void rangeUpdate(int *segTree, int *lazyTree, int index, int start, int end, int
 }
 
 int queryLazy(int *segTree, int *lazyTree, int index, int start, int end, int requiredStart, int requiredEnd) {
-
-    //check for suspended update and if there update
+    // handle lazy
     if (lazyTree[index] != 0) {
-        segTree[index] = (end - start + 1) * lazyTree[index];
+        int range = end - start + 1;
+        int value = range * lazyTree[index];
+        segTree[index] += value;
+
         if (start != end) {
             lazyTree[2*index+1] += lazyTree[index];
             lazyTree[2*index+2] += lazyTree[index];
         }
+
         lazyTree[index] = 0;
     }
 
     // completely inside
-    if (requiredStart <= start && requiredEnd >= end) {
+    if (start >= requiredStart && end <= requiredEnd) {
         return segTree[index];
     }
 
@@ -111,11 +125,13 @@ int queryLazy(int *segTree, int *lazyTree, int index, int start, int end, int re
     if (start > requiredEnd || end < requiredStart) {
         return 0;
     }
-    
-    // Overlap both Partial and Fully
+
+    // overlapping partially or fully
     int mid = (start + end) / 2;
-    int left = queryLazy(segTree, lazyTree, 2*index+1, start, mid, requiredStart, requiredEnd);
-    int right = queryLazy(segTree, lazyTree, 2*index+2, mid + 1, end, requiredStart, requiredEnd);
+
+    int left = querySegmentTree(segTree,2*index+1,start,mid,requiredStart,requiredEnd);
+    int right = querySegmentTree(segTree,2*index+2,mid+1,end,requiredStart,requiredEnd);
+
     return left + right;
 }
 
@@ -133,7 +149,7 @@ int main() {
 
     int *segTree = new int[4*n];
     int *lazyTree = new int[4*n]();
-    buildSegmentTree(arr, segTree, 0, 0, n-1);
+    buildSegmentTree(arr, segTree, 0, n-1, 0);
 
     int q;
     cout << "Enter the number of queries-";
@@ -159,7 +175,7 @@ int main() {
     // Lazy Propogations
     rangeUpdate(segTree, lazyTree, 0, 0, n-1, 0, 2, 1);
 
-    cout << "The maximum of elements in the range [" << 0 << ", " << 9 << "] is " 
+    cout << "The sum of elements in the range [" << 0 << ", " << 9 << "] is " 
              << queryLazy(segTree, lazyTree, 0, 0, n-1, 0, 9) << endl;
 
 
